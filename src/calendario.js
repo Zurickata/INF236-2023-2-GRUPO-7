09485a9ec537fc36090548279bd5038b4194c8ad
Javiwas <javiera.osoriom@usm.cl>
2023-11-25 20:14:13 -0300

    toi cansa

--- src/calendario.js
+++ src/calendario.js
@@ -1,19 +1,49 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './calendario.css';
 
 const Calendario = () => {
   const [date, setDate] = useState(new Date());
+  const [mostrarCalendario, setMostrarCalendario] = useState(false);
+  const [nombre, setNombre] = useState('');
 
-  const onChange = (newDate) => {
+  const abrirCalendario = () => {
+    setMostrarCalendario(true);
+  };
+
+  const cerrarCalendario = () => {
+    setMostrarCalendario(false);
+  };
+
+  const onChange = newDate => {
     setDate(newDate);
   };
 
   return (
     <div>
-      <h1>Calendario</h1>
-      <Calendar onChange={onChange} value={date} />
-      <p>Fecha seleccionada: {date.toDateString()}</p>
+      <div className='cuadrado'>
+        <h1>Disponibilidad</h1>
+        <div className='casillap' id='casillap'>
+          <label>
+            <span>Calendario</span>
+            <input
+              type="text"
+              className="casillap"
+              value={nombre}
+              onChange={e => setNombre(e.target.value)}
+              onFocus={abrirCalendario}
+            />
+          </label>
+          {mostrarCalendario && (
+            <div>
+              <Calendar onChange={onChange} value={date} />
+              <button onClick={cerrarCalendario}>Cerrar</button>
+              <p>Fecha seleccionada: {date.toDateString()}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
