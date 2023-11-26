034df20a7ee662afcd3c05193afba503cb4159b5
Javiwas <javiera.osoriom@usm.cl>
2023-11-25 21:59:47 -0300

    calendario terminado

--- src/calendario.js
+++ src/calendario.js
@@ -3,10 +3,37 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendario.css';
 
+const HoraSelector = ({ hora, setHora }) => {
+  const handleChange = event => {
+    setHora(event.target.value);
+  };
+
+  return (
+    <select value={hora} onChange={handleChange}>
+      <option value="08:00">08:00</option>
+      <option value="09:00">09:00</option>
+      <option value="10:00">10:00</option>
+      <option value="11:00">11:00</option>
+      <option value="12:00">12:00</option>
+      <option value="13:00">13:00</option>
+      <option value="14:00">14:00</option>
+      <option value="15:00">15:00</option>
+      <option value="16:00">16:00</option>
+      <option value="17:00">17:00</option>
+      <option value="18:00">18:00</option>
+      <option value="19:00">19:00</option>
+      <option value="20:00">20:00</option>
+     
+    </select>
+  );
+};
+
 const Calendario = () => {
   const [date, setDate] = useState(new Date());
   const [mostrarCalendario, setMostrarCalendario] = useState(false);
   const [nombre, setNombre] = useState('');
+  const [horaSeleccionada, setHoraSeleccionada] = useState('08:00');
+  const [horaHabilitada, setHoraHabilitada] = useState(false);
 
   const abrirCalendario = () => {
     setMostrarCalendario(true);
@@ -15,9 +42,24 @@ const Calendario = () => {
   const cerrarCalendario = () => {
     setMostrarCalendario(false);
   };
-
-  const onChange = newDate => {
+  const onChangeFecha = newDate => {
     setDate(newDate);
+    setHoraHabilitada(true);
+  
+    const dayNumber = newDate.getDate();
+    const monthNumber = newDate.getMonth() + 1; 
+    const yearNumber = newDate.getFullYear();
+  
+  
+    const formattedDate = `${yearNumber}-${monthNumber.toString().padStart(2, '0')}-${dayNumber.toString().padStart(2, '0')}`;
+  
+   
+    setNombre(formattedDate);
+  };
+  
+
+  const onChangeHora = hora => {
+    setHoraSeleccionada(hora);
   };
 
   return (
@@ -37,37 +79,23 @@ const Calendario = () => {
           </label>
           {mostrarCalendario && (
             <div>
-              <Calendar onChange={onChange} value={date} />
+              <Calendar onChange={onChangeFecha} value={date} />
               <button onClick={cerrarCalendario}>Cerrar</button>
               <p>Fecha seleccionada: {date.toDateString()}</p>
             </div>
           )}
-
         </div>
         <div className='casillap' id='casillap'>
           <label>
             <span>Horario</span>
-            <input
-              type="text"
-              className="casillap"
-              value={nombre}
-              onChange={e => setNombre(e.target.value)}
-              onFocus={abrirCalendario}
-            />
+            {horaHabilitada && (
+              <HoraSelector hora={horaSeleccionada} setHora={onChangeHora} />
+            )}
           </label>
-          {mostrarCalendario && (
-            <div>
-              <Calendar onChange={onChange} value={date} />
-              <button onClick={cerrarCalendario}>Cerrar</button>
-              <p>Hora seleccionada: {date.toDateString()}</p>
-            </div>
-          )}
-          
         </div>
-        <div class= "boton" id = "boton">
-            <input type="submit" className="button1" value="Confirmar fecha" />
-          </div>
-
+        <div className="boton" id="boton">
+          <input type="submit" className="button1" value="Agendar fecha" />
+        </div>
       </div>
     </div>
   );
